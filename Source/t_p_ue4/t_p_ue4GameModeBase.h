7dad22ec5cf10d16b1267379734fd165fd3bb080
Chris <sing.christian@gmail.com>
2018-09-21 07:33:36 -0700

    added new unreal project

---
+++ Source/t_p_ue4/t_p_ue4GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "t_p_ue4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class T_P_UE4_API At_p_ue4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
