94dfb582fb38972a0226845c58483c2c8f67e39a
Chris <sing.christian@gmail.com>
2018-09-22 01:08:30 -0700

    added four materials

--- Source/t_p_ue4/block.cpp
+++ Source/t_p_ue4/block.cpp
@@ -3,6 +3,7 @@
 #include "block.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Components/StaticMeshComponent.h"
+#include "Materials/MaterialInstance.h"
 
 
 // Sets default values
@@ -12,8 +13,18 @@ Ablock::Ablock()
 	struct FConstructorStatics
 	{
 		ConstructorHelpers::FObjectFinderOptional<UStaticMesh> cubeMesh;
+		ConstructorHelpers::FObjectFinderOptional<UMaterial> m_Base;
+		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> m_Blue;
+		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> m_Green;
+		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> m_Orange;
+		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> m_Red;
 		FConstructorStatics()
 			: cubeMesh(TEXT("/Game/Shapes/Sh_Cube.Sh_Cube"))
+			, m_Base(TEXT("/Game/Materials/M_BasicWall.M_BasicWall"))
+			, m_Blue(TEXT("/Game/Materials/M_Blue.M_Blue"))
+			, m_Green(TEXT("/Game/Materials/M_Blue.M_Green"))
+			, m_Orange(TEXT("/Game/Materials/M_Blue.M_Orange"))
+			, m_Red(TEXT("/Game/Materials/M_Blue.M_Red"))
 		{
 		}
 	};
@@ -30,10 +41,19 @@ Ablock::Ablock()
 	// Create static mesh component
 	blockMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BlockMesh0"));
 	blockMesh->SetupAttachment(root);
-
+	// Set start parameter
 	blockMesh->SetStaticMesh(ConstructorStatics.cubeMesh.Get());
 	blockMesh->SetRelativeRotation(FRotator(45.f, -90.f, 45.f));
 	blockMesh->SetRelativeLocation(FVector(-35.f, -25.f, 0.f));
+
+	blockMesh->SetMaterial(0, ConstructorStatics.m_Blue.Get());
+
+	// Save a pointer to the materials
+	m_Base = ConstructorStatics.m_Base.Get();
+	m_Blue = ConstructorStatics.m_Blue.Get();
+	m_Green = ConstructorStatics.m_Green.Get();
+	m_Orange = ConstructorStatics.m_Orange.Get();
+	m_Red = ConstructorStatics.m_Red.Get();
 	
 
 }
--- Source/t_p_ue4/block.h
+++ Source/t_p_ue4/block.h
@@ -32,5 +32,25 @@ public:
 	/** StaticMesh component for the clickable block */
 	UPROPERTY(EditAnywhere)
 		class UStaticMeshComponent* blockMesh;
+
+	/** Pointer to white material used on the focused block */
+	UPROPERTY()
+		class UMaterial* m_Base;
+
+	/** Pointer to blue material used on inactive blocks */
+	UPROPERTY()
+		class UMaterialInstance* m_Blue;
+
+	/** Pointer to green material used on active blocks */
+	UPROPERTY()
+		class UMaterialInstance* m_Green;
+
+	/** Pointer to orange material used on active blocks */
+	UPROPERTY()
+		class UMaterialInstance* m_Orange;
+
+	/** Pointer to red material used on active blocks */
+	UPROPERTY()
+		class UMaterialInstance* m_Red;
 	
 };
